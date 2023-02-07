2ed58caeb5da77560a8d8ec5aa191857c3f51791
pakhyeonjong <phj799@naver.com>
2023-02-08 01:35:17 +0900

    온라인 과제 3-4까지

--- 온라인과제/javascript/1-7.js
+++ 온라인과제/javascript/1-7.js
@@ -1,1 +1,13 @@
 const nums = [1, 2, 3, 4]
+
+// map
+const numsMap = nums.map((num) => num**3);
+
+// reduce
+const numsReduce = nums.reduce((acc, cur) => {
+  acc.push(cur**3);
+  return acc;
+}, [])
+
+
+//
